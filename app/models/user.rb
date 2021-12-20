840d9f43f810944a0273b9579c3a3173df506ce5
MashiKimura <kandai303.m.k@gmail.com>
2021-12-20 17:36:02 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,        presence: true
+  validates :last_name,       presence: true
+  validates :first_name,      presence: true
+  validates :last_name_ruby,  presence: true
+  validates :first_name_ruby, presence: true
+  validates :birth_day,       presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
+  NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
+  validates_format_of :last_name, with: NAME_REGEX
+  validates_format_of :first_name, with: NAME_REGEX
+
+  NAME_RUBY_REGEX = /\A[ァ-ヶ]+\z/.freeze
+  validates_format_of :last_name_ruby, with: NAME_RUBY_REGEX 
+  validates_format_of :first_name_ruby with: NAME_RUBY_REGEX
+  
 end
