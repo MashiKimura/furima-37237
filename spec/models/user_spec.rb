1746ecc26c259ca2f3ace41a233d45cc5ae35fd7
MashiKimura <kandai303.m.k@gmail.com>
2021-12-21 17:30:35 +0900

    userモデル単体テストの実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,46 +8,99 @@ RSpec.describe User, type: :model do
   describe "ユーザー新規登録" do
     context '新規登録ができるとき' do
       it "全ての項目が入力されていれば登録できる" do
+        expect(@user).to be_valid
       end
     end
     context '新規登録できないとき' do
       it "nicknameが空だと登録できない" do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
       it "emailが空では登録できない" do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
       end
       it "emailが重複していると登録できない" do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
       it "emailに@が含んでいないと登録できない" do
+        @user.email
+        @user.email = @user.email.gsub(/@/,'')
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
       end
       it "passwordが空では登録できない" do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
       end
-      it "passwordは6文字以上でなければ登録できない" do
+      it "passwordは5文字以下では登録できない" do
+        @user.password = 'a1234'
+        @user.password_confirmation = 'a1234'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
       it "passwordが半角英数字混合でなければ登録できない" do
+        @user.password = '123456'
+        @user.password_confirmation = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid')
       end
       it "passwordとpassword_confirmationが一致していないと登録できない" do
+        @user.password = 'a12345'
+        @user.password_confirmation = 'a123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
       it "last_nameが空では登録できない" do
+        @user.last_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name can't be blank")
       end
       it "first_nameが空では登録できない" do
+        @user.first_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank")
       end
       it "last_name_rubyが空では登録できない" do
+        @user.last_name_ruby = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name ruby can't be blank")
       end
       it "first_name_rubyが空では登録できない" do
+        @user.first_name_ruby = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name ruby can't be blank")
       end
       it "last_nameは全角(漢字・ひらがな・カタカナ)でなければ登録できない" do
+        @user.last_name = '山田a'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name is invalid")
       end
       it "first_nameは全角(漢字・ひらがな・カタカナ)でなければ登録できない" do
+        @user.first_name = '太郎a'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name is invalid")
       end
-      it "nameが空では登録できない" do
+      it "last_name_rubyは全角(カタカナ)でなければ登録できない" do
+        @user.last_name_ruby = '山田'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name ruby is invalid")
       end
-      it "nameが空では登録できない" do
-      end
-      it "last_nameは全角(カタカナ)でなければ登録できない" do
-      end
-      it "first_nameは全角(カタカナ)でなければ登録できない" do
+      it "first_name_rubyは全角(カタカナ)でなければ登録できない" do
+        @user.first_name_ruby = '太郎'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name ruby is invalid")
       end
       it "birth_dayが空では登録できない" do
+        @user.birth_day = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birth day can't be blank")
       end
     end
   end
