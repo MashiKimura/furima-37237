fa443efed646f772f80bd749347df0a9e09f2dcf
MashiKimura <kandai303.m.k@gmail.com>
2021-12-22 15:26:25 +0900

    商品出品機能　データ保管　実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :delivery
+  belongs_to :prefecture
+  belongs_to :shipping_day
 
   has_one_attached :image
 
   with_options presence: true do
     validates :name
     validates :explanation
-    validates :category_id
+    validates :category_id, numericality: { other_than: 1 }
     validates :situation_id
     validates :delivery_id
     validates :prefecture_id
