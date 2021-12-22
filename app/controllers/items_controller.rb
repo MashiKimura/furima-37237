bda2da810094e12a7f7c0f908cf496f299724484
MashiKimura <kandai303.m.k@gmail.com>
2021-12-22 15:50:26 +0900

    商品出品画面への遷移にはログインが必要

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,10 @@ class ItemsController < ApplicationController
   def index
   end
   def new
-    @item = Item.new
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+      @item = Item.new
   end
   def create
     @item = Item.new(item_params)
