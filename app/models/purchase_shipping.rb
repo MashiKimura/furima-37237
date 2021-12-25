6b1a76c2c44ff8c4d4c08a568721ec797177b6c4
MashiKimura <kandai303.m.k@gmail.com>
2021-12-25 17:26:53 +0900

    purchase_shippingモデルの作成

---
+++ app/models/purchase_shipping.rb
@@ -0,0 +1,26 @@
+class PurchaseShipping
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :street_address, :building, :tel
+
+  # ここにバリデーションの処理を書く
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postcode
+    validates :prefecture_id
+    validates :city
+    validates :street_address
+    validates :tel
+    validates :purchase_item_id
+  end
+  end
+
+  def save
+    # 各テーブルにデータを保存する処理を書く
+    purchase_item = PurchaseItem.create(user_id: user_id, item_id: item_id)
+
+    ShippingItem.create(postcode: postcode, prefecture_id: prefecture_id, city: city, street_address: street_address, building: building, tel: tel, purchase_item_id: purchase_item.id)
+    
+  end
+end
+end
